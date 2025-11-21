78888e35590542e858fbc5a00d0138cc1f8c85dc
achraf <achrafsikal@gmail.com>
2025-11-21 12:27:55 +0100

    chor: Update appConfiguration to migrate twords production database

--- src/main/java/com/tricol/supplier_order/SupplierOrderApplication.java
+++ src/main/java/com/tricol/supplier_order/SupplierOrderApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SupplierOrderApplication {
 
 	public static void main(String[] args) {
+		String postgresUri = "jdbc:postgresql://ep-fancy-bonus-ahln33hw-pooler.c-3.us-east-1.aws.neon.tech:5432/tricol?sslmode=require&channel_binding=require";
+		System.setProperty("spring.datasource.url", postgresUri);
+		System.setProperty("spring.datasource.username", "neondb_owner");
+		System.setProperty("spring.datasource.password", "npg_v9BVOLc3dKnR");
+
 		SpringApplication.run(SupplierOrderApplication.class, args);
 	}
 
